2d3c4632ee446a46d5efaaedf756996a96cf0a7f
Crypto Minion <154598612+jrwbabylonchain@users.noreply.github.com>
2024-05-27 20:38:48 +1000

    fix: issue with delegation endpoint swagger (#144)

--- internal/api/handlers/delegation.go
+++ internal/api/handlers/delegation.go
@@ -12,7 +12,7 @@ import (
 // @Param staking_tx_hash_hex query string true "Staking transaction hash in hex format"
 // @Success 200 {object} PublicResponse[services.DelegationPublic] "Delegation"
 // @Failure 400 {object} types.Error "Error: Bad Request"
-// @Router /v1/delegation/ [get]
+// @Router /v1/delegation [get]
 func (h *Handler) GetDelegationByTxHash(request *http.Request) (*Result, *types.Error) {
 	stakingTxHash, err := parseTxHashQuery(request, "staking_tx_hash_hex")
 	if err != nil {
