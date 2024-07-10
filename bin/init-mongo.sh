550c31454225afb0bf4d942c000d103d5c73f1e8
wjrjerome <wjrjerome@babylonchain.io>
2024-07-10 22:51:21 +1000

    fix: add mongo auth connection

--- bin/init-mongo.sh
+++ bin/init-mongo.sh
@@ -12,6 +12,16 @@ mongosh --eval "rs.initiate({_id: 'RS', members: [{ _id: 0, host: 'localhost:270
 # Wait for replica set to initiate
 sleep 5
 
+# Create the root user
+mongosh --eval "
+db = db.getSiblingDB('admin');
+db.createUser({
+  user: 'root',
+  pwd: 'example',
+  roles: [{ role: 'root', db: 'admin' }]
+});
+"
+
 # Create the necessary indexes
 mongosh --eval "
 db = db.getSiblingDB('staking-api-service');
